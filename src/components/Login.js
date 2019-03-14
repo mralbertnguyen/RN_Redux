c7b6f597cfde1f41bbc6b5c10fc739bdbc06f47d
AlbertNguyen <ndttam.dev@gmail.com>
2019-03-14 14:08:14 +0700

    add style folder

---
+++ src/components/Login.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+import {
+    View,
+    Text
+} from 'react-native';
+
+/**
+ * Login page structures
+ * Logo
+ * Input form : 
+ *  - User name
+ *  - Password
+ * Check box remember
+ * Button form: 
+ *  - Login button
+ *  - Sign in button
+ */
+class Login extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Login Page</Text>
+            </View>
+        );
+    }
+}
